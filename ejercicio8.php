28f1f5d6f97d7e114b12e280a5b8396cef9ca4bd
nabunave <toto_saynomore@hotmail.com>
2023-10-19 19:58:09 -0300

    Update ejercicio8.php

--- ejercicio8.php
+++ ejercicio8.php
@@ -5,3 +5,49 @@
 pantalla, incluyendo el color.-->
 
 <?php
+
+trait Color
+{
+    private $color;
+
+    public function setColor($color)
+    {
+        $this->color = $color;
+    }
+
+    public function getColor()
+    {
+        return $this->color;
+    }
+
+    public function imprimirDatos()
+    {
+        echo "Color: " . $this->color;
+    }
+}
+
+class Automovil
+{
+    use Color;
+    public function imprimirDatosAutomovil()
+    {
+        echo "Este automóvil tiene el color: " . $this->getColor();
+    }
+}
+
+class Bicicleta
+{
+    use Color;
+    public function imprimirDatosBicicleta()
+    {
+        echo "Esta bicicleta tiene el color: " . $this->getColor();
+    }
+}
+
+$auto = new Automovil();
+$auto->setColor("Rojo");
+$auto->imprimirDatosAutomovil();
+
+$bici = new Bicicleta();
+$bici->setColor("Verde");
+$bici->imprimirDatosBicicleta();
